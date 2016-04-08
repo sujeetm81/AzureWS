04c818bca0ca7adedd52588fb141e7aabf4afadf
sujeetm81 <sujeetmehta@hotmail.com>
2016-04-07 22:14:08 -0700

    test
    test message

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var zmq = require('zmq');
+console.log('hello world');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Sujeet M and Azure Web Apps!</h1>');
+    res.end('<h1>Hello Sujeet Mehta and Azure Web Apps!</h1>');
 }).listen(process.env.PORT); 
